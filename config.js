1a2cbfbad1fb1910c05ef5e55ade590c28ae550d
Richard Alex <richard.gms.diogo@gmail.com>
2025-11-14 01:17:20 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js - cliente Supabase centralizado
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+// Configuração do projeto Supabase (dados do seu projeto)
+const SUPABASE_URL = "https://tsdrlbkrkjaxzpdxtmoa.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzZHJsYmtya2pheHpwZHh0bW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxMjY0NjYsImV4cCI6MjA3NzcwMjQ2Nn0.Jx0ot29QIb2bi-wcTL6T69J6oBHoEFbR237Rtf3MO0g";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+export { SUPABASE_URL, SUPABASE_ANON_KEY };
